30e4a2c3f8f49e04111ccaff5b531d10302f3309
Abderrahmane <foutouhabderrahman8@gmail.com>
2025-07-09 10:44:08 +0100

    Initial commit

---
+++ src/main/java/ma/bankatiii/cih/CihApplication.java
@@ -0,0 +1,13 @@
+package ma.bankatiii.cih;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CihApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CihApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/bankatiii/cih/CihApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.bankatiii.cih;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CihApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
