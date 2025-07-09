ea1a0ccedaa252f25a94e36463891445fce02bc1
Abderrahmane <foutouhabderrahman8@gmail.com>
2025-07-09 14:42:32 +0100

    Mise à jour du projet : ajout/modif fichiers

--- src/main/java/ma/bankatiii/cih/models/agent.java
+++ src/main/java/ma/bankatiii/cih/models/agent.java
@@ -1,3 +1,41 @@
 package ma.bankatiii.cih.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class agent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom ;
+    private String prenom ;
+    private String adresse ;
+    private String email ;
+    private String password ;
+
+    public agent() {}
+    public agent(String nom, String prenom, String adresse, String email, String password) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.adresse = adresse;
+        this.email = email;
+        this.password = password;
+    }
+    public Long getId() {return id;}
+    public void setId(Long id) {this.id = id;}
+    public String getNom() {return nom;}
+    public void setNom(String nom) {this.nom = nom;}
+    public String getPrenom() {return prenom;}
+    public void setPrenom(String prenom) {this.prenom = prenom;}
+    public String getAdresse() {return adresse;}
+    public void setAdresse(String adresse) {this.adresse = adresse;}
+    public String getEmail() {return email;}
+    public void setEmail(String email) {this.email = email;}
+    public String getPassword() {return password;}
+    public void setPassword(String password) {this.password = password;}
+
 }
